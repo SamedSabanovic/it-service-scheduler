5a34a3cdeeba1b58d7b70d4dca4e3a6d9bdb6180
Samke <sabanovic.samed@gmail.com>
2026-05-03 14:55:13 +0200

    SJF Algoritam implement

--- it-service-scheduler.py
+++ it-service-scheduler.py
@@ -1,7 +1,6 @@
 import os
 import time
 
-
 def ocisti_ekran():
     # Funkcija za brisanje terminala kako bi meni uvijek bio na vrhu
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -29,7 +28,7 @@ def unos_podataka(ima_priority = False):
         # Vrijeme dolaska: npr. 0 ako je narudžba tu odmah, ili 5 ako stiže kasnije
         dolazak = int(input("Vrijeme dolaska narudžbe (min): "))
         # Burst time: koliko minuta nam treba da završimo posao
-        trajanje = int(input("Vrijeme pripreme narudžbe (min): "))
+        trajanje = int(input("Vrijeme pripreme narudžbe (min): ")) 
         
         #isti fazon kao i struct u c++, inicijalizacija strukture(rjecnika) za jedan tiket
         tiket = {
@@ -49,6 +48,76 @@ def unos_podataka(ima_priority = False):
         lista_tiketa.append(tiket)
     return lista_tiketa
 
+def SJFAlgorithm(lista_tiketa): 
+    trenutno_vrijeme = 0 # alocirano vrijeme gdje mjerimo sve operacije /poredimo ih s ovom nulom koja raste
+    zavrseni_tiketi = [] #appendaju se zavrseni procesi nakon obrade u ovu listu
+
+    while len(lista_tiketa) > 0:
+        dostupni = [] #procesi koji su vec stigli u sistem
+        for t in lista_tiketa:
+            if t["dolazak"] <= trenutno_vrijeme: 
+                dostupni.append(t) #prolazak kroz citavu listu tiketa, provjeravamo da li je vrijeme dolaska manje ili vece od 
+                #trenutnog vremena, ako jeste, dodajemo ga u privremenu listu dostupnih procesa koji su stigli u sistem i cekaju svoj red
+            #ovo fakticki simulira kao "fizicki red" u cekaonici, samo oni koji su prisutni ulaze u izbor, 
+            #npr ako je t["dolazak"] = 12, a trenutno_vrijeme = 10, program "vidi" da 
+            #jos nije usao u procese i ignorise ga dok trenutno_vrijeme ne otkuca na 12
+
+        if len(dostupni) == 0: #ako niko nije stigao, vrijeme se mora nastaviti otkucavati, tako da pse povecava za 1
+            trenutno_vrijeme+=1 #povecava se za jednu jedinicu vremena dok se neko ne pojavi
+            continue #vracanje na pocetak while petlje da provjerimo je li iko stigao i ko je
+        
+        najkraci = dostupni[0]
+        for t in dostupni:
+            if t["trajanje"] < najkraci["trajanje"]: najkraci = t
+        #uzimamo (pretpostavljamo) da je najkraci proces prvi u nizu dostupnih, 
+        # i onda provjeravamo prolazimo kroz citavu listu dostupnih 
+        #provjeravamo da li postoji ikakav proces kojem je trajanje (burst time) manji od zadanog, ako jeste on postaje novi najkraci
+
+        trenutno_vrijeme += najkraci["trajanje"] # -> non-preemptive (znaci da kada jednom se odlucimo za rasporedjivanje procesa - on se mora uraditi do kraja bez prekida)
+        #npr ako je trenutno_vrijeme bio na 5.minuti, a usluga traje 10 minuta -> automatski ih sabiramo 
+        # i postavljamo trenutno_vrijeme na 15.minutu (to znaci da je procesor bio zauzet cijelo to vrijeme i nije mogao gledati druge narudzbe)
+    
+        najkraci["kraj"] = trenutno_vrijeme #ovdje upisujemo tacno kada je klijent dobio uslugu
+
+        najkraci["tat"] = najkraci["kraj"] - najkraci["dolazak"] #tat - ukupno vrijeme koje je proces proveo u sistemu (od ulasksa od zavrsetka)
+        #npr. klijent je poslao request u 5.minuti (dolazak), a usluga stize tek u 20.minuti(kraj)
+        # njegov turnaround time (tat) je 20-5 = 15
+        najkraci["wt"] = najkraci["tat"] - najkraci["trajanje"] #wt - waiting time(vrijeme cekanja) 
+        #npr. klijent je ukupno bio u sistemu 15 minuta (tat), a sama priprema traje 10 minuta (trajanje)
+        #to znaci da 5 minuta je samo chillao i cekao da neko uopste pocne raditi na njegovom requestu
+
+        zavrseni_tiketi.append(najkraci) # kada imamo zavrsena popunjena sva polja, prebacujemo privremenu listu najkraci u listu zavrsenih poslova
+        lista_tiketa.remove(najkraci) #brise taj tiket iz glavne liste
+    return zavrseni_tiketi
+
+def ispis(rezultati):
+    print("\n" + "="*30)
+    print("       FINALNI IZVJEŠTAJ")
+    print("="*30)
+    
+    for r in rezultati:
+        # Provjeri da li su ključevi tačni: 'id', 'dolazak', 'trajanje', 'kraj', 'tat', 'wt'
+        print(f"ID: {r['id']} | "
+              f"Dolazak: {r['dolazak']} min | "
+              f"Trajanje: {r['trajanje']} min | "
+              f"Kraj: {r['kraj']} | "
+              f"TAT: {r['tat']} | "
+              f"WT: {r['wt']}")
+    
+    suma_tat = 0
+    suma_wt = 0
+    n = len(rezultati)
+    
+    for r in rezultati:
+        suma_tat += r['tat']
+        suma_wt += r['wt']
+    
+    print("-" * 30)
+    # Prikaz prosjeka na dvije decimale
+    print(f"Prosječan TAT: {suma_tat / n:.2f} min")
+    print(f"Prosječan WT: {suma_wt / n:.2f} min")
+    input("\nPrisitni bilo sta za nastavak...")
+
 def main(): 
     while True:
         ocisti_ekran()
@@ -60,6 +129,8 @@ def main():
             ocisti_ekran()
             print("--- SJF (Non-Preemptive) ---")
             tiketi = unos_podataka(ima_priority=False)
+            rezultat = SJFAlgorithm(tiketi)
+            ispis(rezultat)
             
         elif izbor == '2':
             ocisti_ekran()
